a4ecd468e4ed5b0565793d79070dc09f3cfe6a96
ayindeholdings <ayindeholdings@gmail.com>
2026-04-07 23:48:23 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "AyindeHoldings Notary Services",
+  title: "AY Holdings Notary Services",
   description: "Mobile notary services in Gainesville, Florida.",
 };
 
